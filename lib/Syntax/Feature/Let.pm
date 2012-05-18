d03b42d7d4fcc3b0bb750e631d6e741cbb4adac3
Robert Sedlacek <r.sedlacek@shadowcat.co.uk>
2012-05-19 01:02:21 +0200

    Distribution created

---
+++ lib/Syntax/Feature/Let.pm
@@ -0,0 +1,20 @@
+use strictures 1;
+
+# ABSTRACT:
+
+package Syntax::Feature::Let;
+
+use syntax qw( simple/v2 );
+use namespace::clean;
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
